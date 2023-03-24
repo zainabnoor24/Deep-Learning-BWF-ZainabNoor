432c84df6d31b4152d9676c33e3531db0f26a9f0
zainabnoor24 <zainabnoorahmad17@gmail.com>
2023-03-24 16:45:54 +0800

    task8

--- Task8/classes.py
+++ Task8/classes.py
@@ -147,3 +147,75 @@ restaurant.set_number_served(22)
 restaurant.customer_numbers()
 restaurant.increment_number_served(7)
 restaurant.customer_numbers()
+
+
+
+
+
+
+############9-7
+print("\n9-7-----")
+
+class User:
+    def __init__(self, first_name, last_name, age, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.location = location
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name}, {self.age} years old, from {self.location}")
+        
+    def greet_user(self):
+        print(f"Hello, {self.first_name}!")
+        
+class Admin(User):
+    def __init__(self, first_name, last_name, age, location):
+        super().__init__(first_name, last_name, age, location)
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+        
+    def show_privileges(self):
+        print(f"Admin privileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+            
+admin = Admin("jnn", "j", 8, "Ny")
+admin.describe_user()
+admin.show_privileges()
+
+
+
+############9-8
+print("\n9-8-----")
+
+class User:
+    def __init__(self, first_name, last_name, age, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.location = location
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name}, {self.age} years old, from {self.location}")
+        
+    def greet_user(self):
+        print(f"Hello, {self.first_name}!")
+        
+class Privileges:
+    def __init__(self):
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+        
+    def show_privileges(self):
+        print(f"Admin privileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, location):
+        super().__init__(first_name, last_name, age, location)
+        self.privileges = Privileges()
+        
+        
+admin = Admin("znb", "noor", 35, "pak")
+admin.describe_user()
+admin.privileges.show_privileges()
